945c9e7a65182bdb106ca572670602f65b500201
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2017-07-13 23:00:10 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const shop = {
+    title: 'Mega',
+    schedule: {
+        mon: {
+            from: '09:00',
+            to: '21:00'
+        },
+        tue: {
+            from: '09:00',
+            to: '21:00'
+        },
+        wed: {
+            from: '09:00',
+            to: '21:00'
+        },
+        thu: {
+            from: '09:00',
+            to: '21:00'
+        },
+        fri: {
+            from: '09:00',
+            to: '21:00'
+        },
+        sat: {
+            from: '10:00',
+            to: '22:00'
+        },
+        sun: {
+            from: '10:00',
+            to: '22:00'
+        }
+    }
+};
+
+console.log(shop.schedule.sat);
